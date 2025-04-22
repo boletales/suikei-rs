6697f5b340ba7d57cb3558ed41c1643e798da873
boletales <30789006+boletales@users.noreply.github.com>
2025-04-23 08:29:29 +0900

    cargo upgrade

--- src/main.rs
+++ src/main.rs
@@ -4,140 +4,160 @@
 #[macro_use]
 extern crate rocket;
 
-extern crate reqwest;
 extern crate base64;
+extern crate reqwest;
 use image::ImageBuffer;
-use rocket::{fs::NamedFile, serde::{Serialize, json::Json}, response::status::NotFound, config::Config};
+use pnet::datalink;
+use rocket::{
+    config::Config,
+    fs::NamedFile,
+    response::status::NotFound,
+    serde::{json::Json, Serialize},
+};
+use std::env;
 use std::{net::IpAddr, path::Path};
 use suikei_rs::*;
-use std::env;
-use pnet::datalink;
 
 #[derive(Serialize)]
-struct Images { 
-  system:  String,
-  river:   String,
-  colored: String,
-  from:    (i32,i32,i32),
-  to:      (i32,i32,i32)
+struct Images {
+    system: String,
+    river: String,
+    colored: String,
+    from: (i32, i32, i32),
+    to: (i32, i32, i32),
 }
 
 #[get("/")]
 async fn index() -> Result<NamedFile, NotFound<String>> {
-  let path = Path::new("docs/").join("index.html");
-  NamedFile::open(&path).await.map_err(|e| NotFound(e.to_string()))
+    let path = Path::new("docs/").join("index.html");
+    NamedFile::open(&path)
+        .await
+        .map_err(|e| NotFound(e.to_string()))
 }
 
 #[get("/api/images/<zoom>/<lat>/<long>/<pow>/<mag>")]
-fn api_images_pow(zoom:i32,lat:f64,long:f64,pow:f64,mag:f64) -> Json<Images> {
-  let zoom =
-    if zoom>14 {
-      14
-    } else {
-      zoom
-    };
-  Json(get_images(zoom,lat,long,1,pow,mag))
+fn api_images_pow(zoom: i32, lat: f64, long: f64, pow: f64, mag: f64) -> Json<Images> {
+    let zoom = if zoom > 14 { 14 } else { zoom };
+    Json(get_images(zoom, lat, long, 1, pow, mag))
 }
 
 #[get("/api/images/<zoom>/<lat>/<long>")]
-fn api_images(zoom:i32,lat:f64,long:f64) -> Json<Images> {
-  let zoom = 
-    if zoom>14 {
-      14
-    } else {
-      zoom
-    };
-  Json(get_images(zoom,lat,long,1,0.2,4.0))
+fn api_images(zoom: i32, lat: f64, long: f64) -> Json<Images> {
+    let zoom = if zoom > 14 { 14 } else { zoom };
+    Json(get_images(zoom, lat, long, 1, 0.2, 4.0))
 }
 
 #[launch]
 fn rocket() -> _ {
-  let mut config = Config::release_default();
-  if get_ip_list().len() > 0 {
-    config.address = get_ip_list()[0];
-  }
-  config.port = 
-    match env::var("PORT").map(|x|x.parse()){
-      Ok(Ok(p)) => p,
-      _             => config.port
+    let mut config = Config::release_default();
+    if get_ip_list().len() > 0 {
+        config.address = get_ip_list()[0];
+    }
+    config.port = match env::var("PORT").map(|x| x.parse()) {
+        Ok(Ok(p)) => p,
+        _ => config.port,
     };
-  config.keep_alive = 0;
-  rocket::custom(config).mount("/", routes![index,api_images_pow,api_images])
+    config.keep_alive = 0;
+    rocket::custom(config).mount("/", routes![index, api_images_pow, api_images])
 }
 
 fn get_ip_list() -> Vec<IpAddr> {
-  let mut ips: Vec<IpAddr> = Vec::new();
-  for interface in datalink::interfaces() {
-    if !interface.ips.is_empty() && interface.is_up() {
-      for ip_net in interface.ips {
-        if ip_net.is_ipv4() && !ip_net.ip().is_loopback() {
-          ips.push(ip_net.ip());
+    let mut ips: Vec<IpAddr> = Vec::new();
+    for interface in datalink::interfaces() {
+        if !interface.ips.is_empty() && interface.is_up() {
+            for ip_net in interface.ips {
+                if ip_net.is_ipv4() && !ip_net.ip().is_loopback() {
+                    ips.push(ip_net.ip());
+                }
+            }
         }
-      }
     }
-  };
-  ips
+    ips
 }
 
-fn get_images(zoom:i32,lat:f64,long:f64,size:i32,pow:f64,mag:f64) -> Images {
-  let data = download_data(zoom, lat, long, size);
-  let table = fix_neibor_table(&data,lowest_neighbor_table(&data));
-  let (system,count,ends) = move_water(&table);
-  let mut rng = rand::thread_rng();
-  let colors:Vec<[u8;3]> = ends.iter().map(|(_,_)| random_color(&mut rng)).collect();
-  let h = data.len();
-  let w = data[0].len();
+fn get_images(zoom: i32, lat: f64, long: f64, size: i32, pow: f64, mag: f64) -> Images {
+    let data = download_data(zoom, lat, long, size);
+    let table = fix_neibor_table(&data, lowest_neighbor_table(&data));
+    let (system, count, ends) = move_water(&table);
+    let mut rng = rand::thread_rng();
+    let colors: Vec<[u8; 3]> = ends.iter().map(|(_, _)| random_color(&mut rng)).collect();
+    let h = data.len();
+    let w = data[0].len();
 
-  let system_colored: Vec<Vec<[u8;3]>> = system.iter().map(|v| v.iter().map( |s|
-    if *s == -1 {
-      [255,255,255]
-    }else{
-      colors[*s as usize]
-    }).collect()).collect();
-  
-  let pretty_count : Vec<Vec<f64>> = count.iter().map(|v| v.iter().map(|c| f64::powf(*c as f64,pow)).collect()).collect();
+    let system_colored: Vec<Vec<[u8; 3]>> = system
+        .iter()
+        .map(|v| {
+            v.iter()
+                .map(|s| {
+                    if *s == -1 {
+                        [255, 255, 255]
+                    } else {
+                        colors[*s as usize]
+                    }
+                })
+                .collect()
+        })
+        .collect();
 
-  let max_pcnt = pretty_count.iter().map(|v| v.iter().fold(0.0/0.0, |m, v| v.max(m))).fold(0.0/0.0, |m, v| v.max(m));
-  let min_pcnt = pretty_count.iter().map(|v| v.iter().fold(0.0/0.0, |m, v| v.min(m))).fold(0.0/0.0, |m, v| v.min(m));
-  
-  
-  let system = ImageBuffer::from_fn(w as u32, h as u32, |x, y| {
-    image::Rgb(system_colored[y as usize][x as usize])
-  });
+    let pretty_count: Vec<Vec<f64>> = count
+        .iter()
+        .map(|v| v.iter().map(|c| f64::powf(*c as f64, pow)).collect())
+        .collect();
 
-  let river = ImageBuffer::from_fn(w as u32, h as u32, |x, y| {
-    let l = f64::floor((f64::min(1.0,pretty_count[y as usize][x as usize] - min_pcnt) / (max_pcnt/mag - min_pcnt)) * 255.0) as u8;
-    image::Rgb([l, l, l])
-  });
+    let max_pcnt = pretty_count
+        .iter()
+        .map(|v| v.iter().fold(f64::NAN, |m, v| v.max(m)))
+        .fold(f64::NAN, |m, v| v.max(m));
+    let min_pcnt = pretty_count
+        .iter()
+        .map(|v| v.iter().fold(f64::NAN, |m, v| v.min(m)))
+        .fold(f64::NAN, |m, v| v.min(m));
 
-  let colored = ImageBuffer::from_fn(w as u32, h as u32, |x, y| {
-    let l = f64_clip((pretty_count[y as usize][x as usize] - min_pcnt) / (max_pcnt/mag - min_pcnt),0.0,1.0);
-    let r = f64_clip(system_colored[y as usize][x as usize][0] as f64,0.0,255.0) as u8;
-    let g = f64_clip(system_colored[y as usize][x as usize][1] as f64,0.0,255.0) as u8;
-    let b = f64_clip(system_colored[y as usize][x as usize][2] as f64,0.0,255.0) as u8;
-    let a = f64_clip(l * 255.0 ,0.0,255.0) as u8;
-    image::Rgba([r, g, b, a])
-  });
+    let system = ImageBuffer::from_fn(w as u32, h as u32, |x, y| {
+        image::Rgb(system_colored[y as usize][x as usize])
+    });
 
-  let rgb_to_base64 = |img :ImageBuffer<image::Rgb<u8>, Vec<u8>>|{
-    let mut buf:Vec<u8> = vec![];
-    image::DynamicImage::ImageRgb8(img).write_to(&mut buf, image::ImageOutputFormat::Png);
-    format!("data:image/png;base64,{}",base64::encode(buf))
-  };
+    let river = ImageBuffer::from_fn(w as u32, h as u32, |x, y| {
+        let l = f64::floor(
+            (f64::min(1.0, pretty_count[y as usize][x as usize] - min_pcnt)
+                / (max_pcnt / mag - min_pcnt))
+                * 255.0,
+        ) as u8;
+        image::Rgb([l, l, l])
+    });
 
-  let rgba_to_base64 = |img :ImageBuffer<image::Rgba<u8>, Vec<u8>>|{
-    let mut buf:Vec<u8> = vec![];
-    image::DynamicImage::ImageRgba8(img).write_to(&mut buf, image::ImageOutputFormat::Png);
-    format!("data:image/png;base64,{}",base64::encode(buf))
-  };
+    let colored = ImageBuffer::from_fn(w as u32, h as u32, |x, y| {
+        let l = f64_clip(
+            (pretty_count[y as usize][x as usize] - min_pcnt) / (max_pcnt / mag - min_pcnt),
+            0.0,
+            1.0,
+        );
+        let r = f64_clip(system_colored[y as usize][x as usize][0] as f64, 0.0, 255.0) as u8;
+        let g = f64_clip(system_colored[y as usize][x as usize][1] as f64, 0.0, 255.0) as u8;
+        let b = f64_clip(system_colored[y as usize][x as usize][2] as f64, 0.0, 255.0) as u8;
+        let a = f64_clip(l * 255.0, 0.0, 255.0) as u8;
+        image::Rgba([r, g, b, a])
+    });
 
-  let (z,x,y) = get_id(zoom, lat, long);
+    let rgb_to_base64 = |img: ImageBuffer<image::Rgb<u8>, Vec<u8>>| {
+        let mut buf: Vec<u8> = vec![];
+        image::DynamicImage::ImageRgb8(img).write_to(&mut buf, image::ImageOutputFormat::Png);
+        format!("data:image/png;base64,{}", base64::encode(buf))
+    };
 
-  Images { 
-    system:  rgb_to_base64(system),
-    river:   rgb_to_base64(river),
-    colored: rgba_to_base64(colored),
-    from:    (z,x-size,y-size),
-    to:      (z,x+size,y+size)
-  }
+    let rgba_to_base64 = |img: ImageBuffer<image::Rgba<u8>, Vec<u8>>| {
+        let mut buf: Vec<u8> = vec![];
+        image::DynamicImage::ImageRgba8(img).write_to(&mut buf, image::ImageOutputFormat::Png);
+        format!("data:image/png;base64,{}", base64::encode(buf))
+    };
+
+    let (z, x, y) = get_id(zoom, lat, long);
+
+    Images {
+        system: rgb_to_base64(system),
+        river: rgb_to_base64(river),
+        colored: rgba_to_base64(colored),
+        from: (z, x - size, y - size),
+        to: (z, x + size, y + size),
+    }
 }
--- src/standalone.rs
+++ src/standalone.rs
@@ -1,122 +1,185 @@
-
 extern crate reqwest;
-use std::collections::VecDeque;
-use std::fs::{self,File};
-use std::io::{Write, BufWriter};
-use std::env;
 use image::ImageBuffer;
-use rand::Rng;
 use rand::rngs::ThreadRng;
+use rand::Rng;
+use std::collections::VecDeque;
+use std::env;
+use std::fs::{self, File};
+use std::io::{BufWriter, Write};
 use suikei_rs::*;
 
-
-
 fn main() {
-  fs::create_dir_all("./data").unwrap();
-  let args : Vec<String> = env::args().collect();
-  let (zoom,lat,long,size) = 
-    match parse_args(args) {
-      //None => (12,35.75,139.1,2),
-      None => panic!("parse error..."),
-      Some(t) => t
+    fs::create_dir_all("./data").unwrap();
+    let args: Vec<String> = env::args().collect();
+    let (zoom, lat, long, size) = match parse_args(args) {
+        //None => (12,35.75,139.1,2),
+        None => panic!("parse error..."),
+        Some(t) => t,
     };
-  //let zoom = 14;
-  //let lat  = 35.425847;
-  //let long = 137.385063;
-  //let size = 4;
+    //let zoom = 14;
+    //let lat  = 35.425847;
+    //let long = 137.385063;
+    //let size = 4;
 
-  let data = download_data(zoom, lat, long, size);
-  let map  = download_map(zoom, lat, long, size);
-  //let data = test_data();
-  println!("downloaded: {}x{}",data.len(),data[0].len());
-  let highest = data.iter().map(|v| v.iter().fold(0.0/0.0, |m, v| v.max(m))).fold(0.0/0.0, |m, v| v.max(m));
-  let lowest  = data.iter().map(|v| v.iter().fold(0.0/0.0, |m, v| v.min(m))).fold(0.0/0.0, |m, v| v.min(m));
-  println!("min: {}m",lowest);
-  println!("max: {}m",highest);
-  let gradient = lowest_neighbor_table2(&data);
-  let light_table = light(&gradient);
-  //let table = lowest_neighbor_table(&data);
-  let table = fix_neibor_table(&data,lowest_neighbor_table(&data));
-  //let table = remove_sizeN_loop(fix_neibor_table(&data, remove_sizeN_loop(fix_neibor_table(&data, remove_sizeN_loop(gradient)))));
-  let (system,count,ends) = move_water(&table);
-  let (_,x,y) = get_id(zoom,lat,long);
-  //write_csv("./data/id.csv"    ,&vec![vec![zoom.to_string(),x.to_string(),y.to_string(),size.to_string()]])  .unwrap_or(());
-  //write_csv("./data/data.csv"  ,&data)  .unwrap_or(());
-  //write_csv("./data/system.csv",&system).unwrap_or(());
-  //write_csv("./data/count.csv" ,&count) .unwrap_or(());
-  //write_csv("./data/light.csv" ,&light_table) .unwrap_or(());
-  //write_csv("./data/ends.csv"  ,&ends.iter().map(|(x,y)| vec![x,y]).collect()) .unwrap_or(());
+    let data = download_data(zoom, lat, long, size);
+    let map = download_map(zoom, lat, long, size);
+    //let data = test_data();
+    println!("downloaded: {}x{}", data.len(), data[0].len());
+    let highest = data
+        .iter()
+        .map(|v| v.iter().fold(f64::NAN, |m, v| v.max(m)))
+        .fold(f64::NAN, |m, v| v.max(m));
+    let lowest = data
+        .iter()
+        .map(|v| v.iter().fold(f64::NAN, |m, v| v.min(m)))
+        .fold(f64::NAN, |m, v| v.min(m));
+    println!("min: {}m", lowest);
+    println!("max: {}m", highest);
+    let gradient = lowest_neighbor_table2(&data);
+    let light_table = light(&gradient);
+    //let table = lowest_neighbor_table(&data);
+    let table = fix_neibor_table(&data, lowest_neighbor_table(&data));
+    //let table = remove_sizeN_loop(fix_neibor_table(&data, remove_sizeN_loop(fix_neibor_table(&data, remove_sizeN_loop(gradient)))));
+    let (system, count, ends) = move_water(&table);
+    let (_, x, y) = get_id(zoom, lat, long);
+    //write_csv("./data/id.csv"    ,&vec![vec![zoom.to_string(),x.to_string(),y.to_string(),size.to_string()]])  .unwrap_or(());
+    //write_csv("./data/data.csv"  ,&data)  .unwrap_or(());
+    //write_csv("./data/system.csv",&system).unwrap_or(());
+    //write_csv("./data/count.csv" ,&count) .unwrap_or(());
+    //write_csv("./data/light.csv" ,&light_table) .unwrap_or(());
+    //write_csv("./data/ends.csv"  ,&ends.iter().map(|(x,y)| vec![x,y]).collect()) .unwrap_or(());
 
-  let mut rng = rand::thread_rng();
-  let colors:Vec<[u8;3]> = ends.iter().map(|(_,_)| random_color(&mut rng)).collect();
+    let mut rng = rand::thread_rng();
+    let colors: Vec<[u8; 3]> = ends.iter().map(|(_, _)| random_color(&mut rng)).collect();
 
-  let h = data.len();
-  let w = data[0].len();
+    let h = data.len();
+    let w = data[0].len();
 
-  let _ = ImageBuffer::from_fn(w as u32, h as u32, |x, y| {
-    image::Rgb(map[y as usize][x as usize])
-  }).save("data/map.png");
+    let _ = ImageBuffer::from_fn(w as u32, h as u32, |x, y| {
+        image::Rgb(map[y as usize][x as usize])
+    })
+    .save("data/map.png");
 
-  let _ = ImageBuffer::from_fn(w as u32, h as u32, |x, y| {
-    let l = ((data[y as usize][x as usize] - lowest) / (highest - lowest) * 255.0) as u8;
-    image::Rgb([l, l, l])
-  }).save("data/height.png");
+    let _ = ImageBuffer::from_fn(w as u32, h as u32, |x, y| {
+        let l = ((data[y as usize][x as usize] - lowest) / (highest - lowest) * 255.0) as u8;
+        image::Rgb([l, l, l])
+    })
+    .save("data/height.png");
 
-  let max_light = light_table.iter().map(|v| v.iter().fold(0.0/0.0, |m, v| v.max(m))).fold(0.0/0.0, |m, v| v.max(m));
-  let min_light = light_table.iter().map(|v| v.iter().fold(0.0/0.0, |m, v| v.min(m))).fold(0.0/0.0, |m, v| v.min(m));
-  let _ = ImageBuffer::from_fn(w as u32, h as u32, |x, y| {
-    let l = f64::floor((f64::min(1.0,light_table[y as usize][x as usize] - min_light) / (max_light - min_light)) * 255.0) as u8;
-    image::Rgb([l, l, l])
-  }).save("data/light.png");
+    let max_light = light_table
+        .iter()
+        .map(|v| v.iter().fold(f64::NAN, |m, v| v.max(m)))
+        .fold(f64::NAN, |m, v| v.max(m));
+    let min_light = light_table
+        .iter()
+        .map(|v| v.iter().fold(f64::NAN, |m, v| v.min(m)))
+        .fold(f64::NAN, |m, v| v.min(m));
+    let _ = ImageBuffer::from_fn(w as u32, h as u32, |x, y| {
+        let l = f64::floor(
+            (f64::min(1.0, light_table[y as usize][x as usize] - min_light)
+                / (max_light - min_light))
+                * 255.0,
+        ) as u8;
+        image::Rgb([l, l, l])
+    })
+    .save("data/light.png");
 
-  let system_colored: Vec<Vec<[u8;3]>> = system.iter().map(|v| v.iter().map( |s|
-    if *s == -1 {
-      [255,255,255]
-    }else{
-      colors[*s as usize]
-    }).collect()).collect();
+    let system_colored: Vec<Vec<[u8; 3]>> = system
+        .iter()
+        .map(|v| {
+            v.iter()
+                .map(|s| {
+                    if *s == -1 {
+                        [255, 255, 255]
+                    } else {
+                        colors[*s as usize]
+                    }
+                })
+                .collect()
+        })
+        .collect();
 
-  let _ = ImageBuffer::from_fn(w as u32, h as u32, |x, y| {
-    image::Rgb(system_colored[y as usize][x as usize])
-  }).save("data/systems.png");
+    let _ = ImageBuffer::from_fn(w as u32, h as u32, |x, y| {
+        image::Rgb(system_colored[y as usize][x as usize])
+    })
+    .save("data/systems.png");
 
-  let pretty_count : Vec<Vec<f64>> = count.iter().map(|v| v.iter().map(|c| f64::powf(*c as f64,0.2)).collect()).collect();
+    let pretty_count: Vec<Vec<f64>> = count
+        .iter()
+        .map(|v| v.iter().map(|c| f64::powf(*c as f64, 0.2)).collect())
+        .collect();
 
-  let max_pcnt = pretty_count.iter().map(|v| v.iter().fold(0.0/0.0, |m, v| v.max(m))).fold(0.0/0.0, |m, v| v.max(m));
-  let min_pcnt = pretty_count.iter().map(|v| v.iter().fold(0.0/0.0, |m, v| v.min(m))).fold(0.0/0.0, |m, v| v.min(m));
-  
-  let _ = ImageBuffer::from_fn(w as u32, h as u32, |x, y| {
-    let l = f64::floor((f64::min(1.0,pretty_count[y as usize][x as usize] - min_pcnt) / (max_pcnt/4.0 - min_pcnt)) * 255.0) as u8;
-    image::Rgb([l, l, l])
-  }).save("data/river.png");
+    let max_pcnt = pretty_count
+        .iter()
+        .map(|v| v.iter().fold(f64::NAN, |m, v| v.max(m)))
+        .fold(f64::NAN, |m, v| v.max(m));
+    let min_pcnt = pretty_count
+        .iter()
+        .map(|v| v.iter().fold(f64::NAN, |m, v| v.min(m)))
+        .fold(f64::NAN, |m, v| v.min(m));
 
-  let _ = ImageBuffer::from_fn(w as u32, h as u32, |x, y| {
-    let l = f64_clip((pretty_count[y as usize][x as usize] - min_pcnt) / (max_pcnt/4.0 - min_pcnt),0.0,1.0);
-    let r = f64_clip(system_colored[y as usize][x as usize][0] as f64,0.0,255.0) as u8;
-    let g = f64_clip(system_colored[y as usize][x as usize][1] as f64,0.0,255.0) as u8;
-    let b = f64_clip(system_colored[y as usize][x as usize][2] as f64,0.0,255.0) as u8;
-    let a = f64_clip(l * 255.0 ,0.0,255.0) as u8;
-    image::Rgba([r, g, b, a])
-  }).save("data/colored.png");
-  
-  let _ = ImageBuffer::from_fn(w as u32, h as u32, |x, y| {
-    let l = f64_clip((pretty_count[y as usize][x as usize] - min_pcnt) / (max_pcnt/4.0 - min_pcnt),0.0,1.0);
-    let r = f64_clip(system_colored[y as usize][x as usize][0] as f64 * l + map[y as usize][x as usize][0] as f64 * (1.0-l) ,0.0,255.0) as u8;
-    let g = f64_clip(system_colored[y as usize][x as usize][1] as f64 * l + map[y as usize][x as usize][1] as f64 * (1.0-l) ,0.0,255.0) as u8;
-    let b = f64_clip(system_colored[y as usize][x as usize][2] as f64 * l + map[y as usize][x as usize][2] as f64 * (1.0-l) ,0.0,255.0) as u8;
-    image::Rgb([r, g, b])
-  }).save("data/result.png");
-  println!("{}",get_url_png_from_ll(zoom,lat,long));
+    let _ = ImageBuffer::from_fn(w as u32, h as u32, |x, y| {
+        let l = f64::floor(
+            (f64::min(1.0, pretty_count[y as usize][x as usize] - min_pcnt)
+                / (max_pcnt / 4.0 - min_pcnt))
+                * 255.0,
+        ) as u8;
+        image::Rgb([l, l, l])
+    })
+    .save("data/river.png");
+
+    let _ = ImageBuffer::from_fn(w as u32, h as u32, |x, y| {
+        let l = f64_clip(
+            (pretty_count[y as usize][x as usize] - min_pcnt) / (max_pcnt / 4.0 - min_pcnt),
+            0.0,
+            1.0,
+        );
+        let r = f64_clip(system_colored[y as usize][x as usize][0] as f64, 0.0, 255.0) as u8;
+        let g = f64_clip(system_colored[y as usize][x as usize][1] as f64, 0.0, 255.0) as u8;
+        let b = f64_clip(system_colored[y as usize][x as usize][2] as f64, 0.0, 255.0) as u8;
+        let a = f64_clip(l * 255.0, 0.0, 255.0) as u8;
+        image::Rgba([r, g, b, a])
+    })
+    .save("data/colored.png");
+
+    let _ = ImageBuffer::from_fn(w as u32, h as u32, |x, y| {
+        let l = f64_clip(
+            (pretty_count[y as usize][x as usize] - min_pcnt) / (max_pcnt / 4.0 - min_pcnt),
+            0.0,
+            1.0,
+        );
+        let r = f64_clip(
+            system_colored[y as usize][x as usize][0] as f64 * l
+                + map[y as usize][x as usize][0] as f64 * (1.0 - l),
+            0.0,
+            255.0,
+        ) as u8;
+        let g = f64_clip(
+            system_colored[y as usize][x as usize][1] as f64 * l
+                + map[y as usize][x as usize][1] as f64 * (1.0 - l),
+            0.0,
+            255.0,
+        ) as u8;
+        let b = f64_clip(
+            system_colored[y as usize][x as usize][2] as f64 * l
+                + map[y as usize][x as usize][2] as f64 * (1.0 - l),
+            0.0,
+            255.0,
+        ) as u8;
+        image::Rgb([r, g, b])
+    })
+    .save("data/result.png");
+    println!("{}", get_url_png_from_ll(zoom, lat, long));
 }
 
-fn parse_args(args : Vec<String>) -> Option<(i32,f64,f64,i32)> {
-  if args.len()>4 {
-    let zoom : Option<i32> = args[1].parse().ok();
-    let lat  : Option<f64> = args[2].parse().ok();
-    let long : Option<f64> = args[3].parse().ok();
-    let size : Option<i32> = args[4].parse().ok();
-    Some((zoom?,lat?,long?,size?))
-  }else{
-    None
-  }
+fn parse_args(args: Vec<String>) -> Option<(i32, f64, f64, i32)> {
+    if args.len() > 4 {
+        let zoom: Option<i32> = args[1].parse().ok();
+        let lat: Option<f64> = args[2].parse().ok();
+        let long: Option<f64> = args[3].parse().ok();
+        let size: Option<i32> = args[4].parse().ok();
+        Some((zoom?, lat?, long?, size?))
+    } else {
+        None
+    }
 }
